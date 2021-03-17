9aa43defef2672472b1d2e78072a283c75ed20f7
sieunhan345 <smallmouse3121@gmail.com>
2021-03-17 08:29:29 +0700

    1sp upload to remote Github

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Cacheplataungam
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHUb!!!");
+        System.out.println("2 simple sentences");
+        System.out.println("2ND UPLOAD FROM LOCAL TO GITHUB");
+    }
+    
+}
